f5446a52c9a1af0fc1c636895f150a7639c8e7ef
dougwells <dougwells@gmail.com>
2017-01-31 07:15:36 -0700

    Flappybird. Add gravity & impulse (up) to bird.

--- flappyBirdClone/GameScene.swift
+++ flappyBirdClone/GameScene.swift
@@ -15,23 +15,27 @@ class GameScene: SKScene {
     var bird = SKSpriteNode()
     var background1 = SKSpriteNode()
     var background2 = SKSpriteNode()
-    var background3 = SKSpriteNode()
     
-    // Like "viewDidLoad" method.  Scene did appear on screen
+    //Build game element textures
+    let birdTexture  = SKTexture(imageNamed: "flappy1.png")
+    let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
+    let backgroundTexture = SKTexture(imageNamed: "bg.png")
+    
+// Like "viewDidLoad" method.  Scene did appear on screen
     override func didMove(to view: SKView) {
         
-        //Build game element textures
-        let birdTexture  = SKTexture(imageNamed: "flappy1.png")
-        let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
-        let backgroundTexture = SKTexture(imageNamed: "bg.png")
+
         
         //.animate --> array of images to scroll thru each time period
-        let animation = SKAction.animate(with: [birdTexture, birdTexture2], timePerFrame: 0.1)
-        let makeBirdFlap = SKAction.repeatForever(animation)
         
-        let moveAction = SKAction.move(by: CGVector(dx:-backgroundTexture.size().width, dy:0), duration: 5)
-        let resetAction = SKAction.move(by: CGVector(dx:backgroundTexture.size().width, dy:0), duration: 0)
-        let moveBackground = SKAction.repeatForever(SKAction.sequence([moveAction, resetAction]))
+            //bird
+                let animation = SKAction.animate(with: [birdTexture, birdTexture2], timePerFrame: 0.1)
+                let makeBirdFlap = SKAction.repeatForever(animation)
+        
+            //background
+                let moveAction = SKAction.move(by: CGVector(dx:-backgroundTexture.size().width, dy:0), duration: 5)
+                let resetAction = SKAction.move(by: CGVector(dx:backgroundTexture.size().width, dy:0), duration: 0)
+                let moveBackground = SKAction.repeatForever(SKAction.sequence([moveAction, resetAction]))
         
         //build element & run animations
         bird = SKSpriteNode(texture: birdTexture)
@@ -42,12 +46,6 @@ class GameScene: SKScene {
         
         background2 = SKSpriteNode(texture: backgroundTexture)
         background2.run(moveBackground)
-        
-        /*
-        background3 = SKSpriteNode(texture: backgroundTexture)
-        background3.run(moveBackground)
-        */
-        
         
         
         //Position element. self=view contrlr, frame= frame in which items are contained, midX & midY = middle
@@ -64,16 +62,6 @@ class GameScene: SKScene {
         background2.size = CGSize(width: backgroundTexture.size().width, height: self.frame.height)
         self.addChild(background2)
         
-        /*
-        background3.zPosition = -1
-        background3.position = CGPoint(x: 2*backgroundTexture.size().width, y: self.frame.midY)
-        background3.size = CGSize(width: backgroundTexture.size().width, height: self.frame.height)
-        self.addChild(background3)
-        */
-        
-        //add bird to the view controller
-        
-        
         self.addChild(bird)
 
 
@@ -82,7 +70,11 @@ class GameScene: SKScene {
     
     //user touches the screen
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: birdTexture.size().height/2)
+        bird.physicsBody!.isDynamic = true  //not needed. default is true
+        bird.physicsBody!.applyImpulse(CGVector(dx: 0, dy: 50))  //move up 50 pixels
+        
     }
     
     //called several times per second (check for collisions, move items, etc
